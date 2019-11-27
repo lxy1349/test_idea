203b8ddadcfc94248bdc847b8730822732313448
lxy1349 <1349626048@qq.com>
2019-11-27 11:17:24 +0800

    第二次修改

--- idea_github/src/Main.java
+++ idea_github/src/Main.java
@@ -3,4 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    void test(){
+        System.out.println(123);
+    }
+    void test1(){
+        System.out.println(123);
+    }
+
 }
